9c67021748ee9fc1d4a3ac1c713c3651d8c9d256
OldGameBox <mr.kumthebest@gmail.com>
2021-05-21 01:55:13 +0300

    New big update!

--- masha.py
+++ masha.py
@@ -1,20 +1,23 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import font
 import webbrowser
 import sys
-import os
+import random
 
 filename = ""
 fileinput = ""
 fontsize = 10
 
-
+bg = "#ffffff"
+fg = "#000000"
 
 
 def getinput():
     global fileinput
-    fileinput = input_text_area.get("1.0",'end-1c')
+    fileinput = input_text_area.get("1.0", 'end-1c')
+
 
 def openfile(*e):
     getinput()
@@ -25,28 +28,30 @@ def openfile(*e):
     filename = filedialog.askopenfilename()
 
     if filename != "":
-        file = open(filename,"r")
+        file = open(filename, "r")
         fileinput = file.read()
         file.close()
         input_text_area.delete(1.0, "end-1c")
         input_text_area.insert("end-1c", fileinput)
-        root.title(filename+" - Masha")
-        lent.set(len(input_text_area.get("1.0",tk.END)))
-        menu.entryconfig(5, label="Lenght :"+str(lent.get()))
+        root.title(filename + " - Masha")
+        lent.set(len(input_text_area.get("1.0", tk.END)))
+        menu.entryconfig(7, label="Lenght :" + str(lent.get()))
+
 
 def openfile_(filename2):
     global filename
     global fileinput
     global root
     filename = filename2
-    file = open(filename,"r")
+    file = open(filename, "r")
     fileinput = file.read()
     file.close()
     input_text_area.delete(1.0, "end-1c")
     input_text_area.insert("end-1c", fileinput)
-    root.title(filename+" - Masha")
-    lent.set(len(input_text_area.get("1.0",tk.END)))
-    menu.entryconfig(5, label="Lenght :"+str(lent.get()))
+    root.title(filename + " - Masha")
+    lent.set(len(input_text_area.get("1.0", tk.END)))
+    menu.entryconfig(7, label="Lenght :" + str(lent.get()))
+
 
 def savefile(*e):
     getinput()
@@ -54,91 +59,163 @@ def savefile(*e):
     global fileinput
 
     if filename != "":
-        root.title(filename+" - Masha")
-        file = open(filename,"w")
+        root.title(filename + " - Masha")
+        file = open(filename, "w")
         file.write(fileinput)
         file.close()
     else:
-        filename = filedialog.asksaveasfilename(title = "Save")
+        filename = filedialog.asksaveasfilename(title="Save")
         if filename != "":
-            root.title(filename+" - Masha")
-            file = open(filename,"w")
+            root.title(filename + " - Masha")
+            file = open(filename, "w")
             file.write(fileinput)
             file.close()
 
+
 def newfile(*e):
     global filename
-    filename = filedialog.asksaveasfilename(title = "New")
+    filename = filedialog.asksaveasfilename(title="New")
 
     if filename != "":
-        root.title(filename+" - Masha")
-        file = open(filename,"w+")
+        root.title(filename + " - Masha")
+        file = open(filename, "w+")
         fileinput = file.read()
         file.close()
         input_text_area.delete(1.0, "end-1c")
         input_text_area.insert("end-1c", fileinput)
+
 
 def saveasfile(*e):
     getinput()
     global filename
     global fileinput
 
-    filename = filedialog.asksaveasfilename(title = "Save as")
+    filename = filedialog.asksaveasfilename(title="Save as")
     if filename != "":
-        root.title(filename+" - Masha")
-        file = open(filename,"w")
+        root.title(filename + " - Masha")
+        file = open(filename, "w")
         file.write(fileinput)
         file.close()
 
+
 def unsaved(event=None):
-    root.title(filename+" - Masha *")
-    lent.set(len(input_text_area.get("1.0",tk.END)))
-    menu.entryconfig(5, label="Lenght :"+str(lent.get()))
+    root.title(filename + " - Masha *")
+    lent.set(len(input_text_area.get("1.0", tk.END)))
+    menu.entryconfig(7, label="Lenght :" + str(lent.get()))
+
 
 def plus(*e):
     global fontsize
-    fontsize+=1
-    input_text_area.configure(font=("Arial",fontsize))
+    fontsize += 1
+    input_text_area.configure(font=("Arial", fontsize))
+
 
 def minus(*e):
     global fontsize
     if fontsize > 1:
-        fontsize-=1
-        input_text_area.configure(font=("Arial",fontsize))
+        fontsize -= 1
+        input_text_area.configure(font=("Arial", fontsize))
+
 
 def pm(event):
     global fontsize
     if event.delta > 0:
-        fontsize+=1
-        input_text_area.configure(font=("Arial",fontsize))
+        fontsize += 1
+        input_text_area.configure(font=("Arial", fontsize))
     elif event.delta < 0:
         if fontsize > 1:
-            fontsize-=1
-            input_text_area.configure(font=("Arial",fontsize))
+            fontsize -= 1
+            input_text_area.configure(font=("Arial", fontsize))
+
 
 def exitc(*e):
     exit()
+
 
 def select_all(event=None):
     input_text_area.tag_add('sel', '1.0', 'end')
     return "break"
 
+
 def copy_lenght(event=None):
     root.clipboard_clear()
     root.clipboard_append(len(fileinput))
 
+
 def web(event=None):
-    webbrowser.open("https://takeb1nzyto.space")
+    webbrowser.open("https://github.com/OldGameBox/Masha-text-editor")
+
 
 def about(event=None):
     messagebox.showinfo("About", "Masha Text Editor created by OldGameBox.\nThank you for using our software!")
+
+
+def tab(arg):
+    input_text_area.insert(tk.INSERT, " " * 4)
+    return 'break'
+
+
+def maria(event=None):
+    fontt = random.choice(fonts)
+    menu.entryconfig(1, font=(fontt, 12))
+    menu.entryconfig(2, font=(fontt, 12))
+    menu.entryconfig(3, font=(fontt, 12))
+    menu.entryconfig(4, font=(fontt, 12))
+    menu.entryconfig(5, font=(fontt, 12))
+    menu.entryconfig(6, font=(fontt, 12))
+    menu.entryconfig(7, font=(fontt, 12))
+    input_text_area.configure(font=(fontt, fontsize))
+
+
+def tabs(event=None):
+    line_text = input_text_area.get("insert linestart", "insert lineend")
+    spaces = 0
+    for letter in line_text:
+        if letter == " ":
+            spaces += 1
+        else:
+            break
+    spaces = spaces // 4
+    try:
+        if line_text[-1] == ":" or line_text[-1] == "{":
+            spaces += 1
+        elif line_text[-2] == ":" and line_text[-1] == " " or line_text[-2] == "{" and line_text[-1] == " ":
+            spaces += 1
+    except:
+        spaces = spaces
+    text = "\n" + "    " * spaces
+    input_text_area.insert("insert", text)
+    return 'break'
+
+
+def back(event=None):
+    if input_text_area.get("insert-4c", "insert") == "    ":
+        input_text_area.delete("insert-4c", "insert")
+        return 'break'
+
+
+def change_theme(event=None):
+    if dark.get():
+        input_text_area.configure(bg=fg, fg=bg)
+        menu.configure(bg=fg, fg=bg)
+        scrollbar.configure(bg=fg)
+        scrollbar2.configure(bg=fg)
+
+    else:
+        input_text_area.configure(bg=bg, fg=fg)
+        menu.configure(bg=bg, fg=fg)
+        scrollbar.configure(bg=bg)
+        scrollbar2.configure(bg=bg)
+
+
 
 root = tk.Tk()
 root.title("Masha")
 root.geometry("650x500")
 root.minsize(300, 150)
 
-
+fonts = font.families()
+print(random.choice(fonts))
 
 menu = tk.Menu(root)
 
@@ -153,13 +230,25 @@ root.bind('<Control-o>', openfile)
 root.bind('<Control-n>', newfile)
 root.bind('<Control-e>', exitc)
 
+scrollbar = tk.Scrollbar(root)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+scrollbar2 = tk.Scrollbar(root, orient="horizontal")
+scrollbar2.pack(side=tk.BOTTOM, fill=tk.X)
 
-
-input_text_area = tk.Text(root, font=("Arial",10),undo=True,autoseparators=True,maxundo=-1)
+input_text_area = tk.Text(root, font=("Arial", 10), undo=True, autoseparators=True, maxundo=-1,
+                          yscrollcommand=scrollbar.set,
+                          xscrollcommand=scrollbar2.set)
+scrollbar.config(command=input_text_area.yview)
+scrollbar2.config(command=input_text_area.xview)
 input_text_area.pack(expand=True, fill='both')
 input_text_area.bind('<<Modified>>', lambda event: unsaved())
+input_text_area.bind("<Tab>", tab)
+input_text_area.bind("<Return>", tabs)
+input_text_area.bind("<BackSpace>", back)
 root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
+
+menu.add_command(label="Masha", font='Arial 12 bold', command=maria)
 
 file_menu = tk.Menu(menu, tearoff=0)
 file_menu.add_command(label="New", accelerator="Ctrl+N", command=newfile)
@@ -179,24 +268,30 @@ edit_menu.add_command(label="Paste", accelerator="Ctrl+V", command=lambda: root.
 edit_menu.add_separator()
 edit_menu.add_command(label='Select All', underline=7, accelerator='Ctrl+A', command=select_all)
 
+theme_menu = tk.Menu(menu, tearoff=0)
+theme_menu.add_command(label="Font")
+dark = tk.IntVar(root,0)
+
+theme_menu.add_checkbutton(label="DarkMode", onvalue=1, offvalue=0, variable=dark, command=change_theme)
+
 about_menu = tk.Menu(menu, tearoff=0)
 about_menu.add_command(label="Website", command=web)
 about_menu.add_command(label="About", command=about)
 
-
 menu.add_cascade(label='File', menu=file_menu)
 menu.add_cascade(label='Edit', menu=edit_menu)
+menu.add_cascade(label='Theme', menu=theme_menu)
 menu.add_cascade(label='About', menu=about_menu)
 menu.add_command(label="|", activebackground=menu.cget("background"))
 
 lent = tk.IntVar(root, 0)
 root.bind('<KeyPress>', unsaved)
-menu.add_command(label="Lenght :"+str(lent.get()), command=copy_lenght)
+menu.add_command(label="Lenght :" + str(lent.get()), command=copy_lenght)
 
 if len(sys.argv) != 1:
     if not "python" in sys.argv[0]:
         if len(sys.argv) > 1:
-            openfile_(sys.argv[len(sys.argv)-1])
+            openfile_(sys.argv[len(sys.argv) - 1])
     else:
         if len(sys.argv) > 2:
             openfile_(sys.argv[len(sys.argv) - 1])
